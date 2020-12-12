5167d7c93de7d485d18a40cbbb58bbf71286e9fb
k1ycee <chiagozieani145@gmail.com>
2020-12-12 09:28:28 +0100

    Added High Yield Dollar Screens moving to integrating API's

---
+++ lib/new_screens/navigation/investments/naira/high_yield_investment_details_naira.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:zimvest/new_screens/navigation/investments/naira/high_yield_investment_naira_name_input.dart';
+import 'package:zimvest/styles/colors.dart';
+import 'package:zimvest/utils/margins.dart';
+import 'package:zimvest/utils/strings.dart';
+import 'package:zimvest/widgets/buttons.dart';
+
+class HighYieldDetails extends StatefulWidget {
+  final String duration;
+
+  const HighYieldDetails({Key key, this.duration}) : super(key: key);
+  static Route<dynamic> route(String dur) {
+    return MaterialPageRoute(
+      builder: (_) => HighYieldDetails(
+        duration: dur,
+      ),
+      settings: RouteSettings(
+        name: HighYieldDetails().toStringShort(),
+      ),
+    );
+  }
+
+  @override
+  _HighYieldDetailsState createState() => _HighYieldDetailsState();
+}
+
+class _HighYieldDetailsState extends State<HighYieldDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.kHighYield,
+      body: Stack(
+        children: [
+          Positioned(
+            top: screenHeight(context) / 10,
+            left: screenWidth(context) / 15,
+            child: IconButton(
+              icon: ImageIcon(
+                AssetImage("images/cancel.png"),
+                color: AppColors.kWhite,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          Positioned(
+            top: screenHeight(context) / 10,
+            left: screenWidth(context) / 15,
+            right: screenWidth(context) / 15,
+            bottom: screenHeight(context) / 1.6,
+            child: SvgPicture.asset("images/money_glass.svg")
+          ),
+          Positioned(
+            top: screenHeight(context) / 2.5,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: screenHeight(context) / 1.5,
+              decoration: BoxDecoration(
+                color: AppColors.kWhite,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 39.0, left: 20),
+                    child: Text(
+                      "Zimvest High Yield Naira ${widget.duration} Days",
+                      style: TextStyle(
+                          fontSize: 15, fontFamily: AppStrings.fontBold),
+                    ),
+                  ),
+                  verticalSpace(18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      "This investment is designed for investors with moderate risk tolerance and a short to medium-term investment horizon.",
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 13, fontFamily: AppStrings.fontLight),
+                    ),
+                  ),
+                  verticalSpace(33),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 19),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/star.png"),
+                                color: AppColors.kPrimaryColor,
+                              ),
+                              Text(
+                                "Earn great returns.",
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: AppStrings.fontLight),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/star.png"),
+                                color: AppColors.kPrimaryColor,
+                              ),
+                              Text(
+                                "Withdraw at the first day of every quarter.",
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: AppStrings.fontLight),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/star.png"),
+                                color: AppColors.kPrimaryColor,
+                              ),
+                              Text(
+                                "Save daily, weekly or monthly.",
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: AppStrings.fontLight),
+                              ),
+                            ],
+                          ),
+                          verticalSpace(112),
+                          Center(
+                            child: PrimaryButtonNew(
+                              title: "Get Started",
+                              onTap: () => Navigator.push(
+                                context,
+                                HighYieldInvestmentNairaUniqueName.route(),
+                              ),
+                            ),
+                          ),
+                          verticalSpace(63),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/new_screens/navigation/investments/naira/high_yield_investment_naira_purchase_source.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:zimvest/new_screens/navigation/investments/naira/investment_summary.dart';
+import 'package:zimvest/styles/colors.dart';
+import 'package:zimvest/utils/margin.dart';
+import 'package:zimvest/utils/margins.dart';
+import 'package:zimvest/utils/strings.dart';
+import 'package:zimvest/widgets/buttons.dart';
+
+class HighYieldInvestmentPurchaseSource extends StatefulWidget {
+  static Route<dynamic> route() {
+    return MaterialPageRoute(
+      builder: (_) => HighYieldInvestmentPurchaseSource(),
+      settings: RouteSettings(
+        name: HighYieldInvestmentPurchaseSource().toStringShort(),
+      ),
+    );
+  }
+
+  @override
+  _HighYieldInvestmentPurchaseSourceState createState() =>
+      _HighYieldInvestmentPurchaseSourceState();
+}
+
+class _HighYieldInvestmentPurchaseSourceState
+    extends State<HighYieldInvestmentPurchaseSource> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.kWhite,
+        title: Text(
+          "Invest",
+          style: TextStyle(
+            fontSize: 13,
+            fontFamily: AppStrings.fontMedium,
+            color: AppColors.kTextColor,
+          ),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: AppColors.kPrimaryColor,
+            ),
+            onPressed: () => Navigator.pop(context)),
+      ),
+      body: Builder(
+        builder: (context) => Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            YMargin(69),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 76),
+              child: Text(
+                "Choose Funding Source",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontFamily: AppStrings.fontBold,
+                  color: AppColors.kTextColor,
+                ),
+              ),
+            ),
+            verticalSpace(52),
+            PaymentSourceButton(
+              onTap: (){
+                print("hmmm");
+                Navigator.push(context, InvestmentSummaryScreenNaira.route());
+              },
+              paymentsource: "Debit Card",
+              image: "card",
+            ),
+            YMargin(25),
+            PaymentSourceButton(
+              onTap: () => Navigator.push(context, InvestmentSummaryScreenNaira.route()),
+              paymentsource: "Wallet",
+              image: "wallet",
+              amount: "${AppStrings.nairaSymbol}30,000,000",
+              color: AppColors.kTextColor,
+            ),
+            YMargin(12),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text(
+                "Funding with your Zimvest Wallet is totally free",
+                style: TextStyle(
+                  fontSize: 13,
+                  fontFamily: AppStrings.fontLight,
+                  color: AppColors.kTextColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
